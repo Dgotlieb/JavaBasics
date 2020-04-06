34733a6d8311d24251a6c448f56055d84c44ef2a
dgotlieb <Dgotlieb8@gmail.com>
2020-04-06 21:39:29 +0300

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,19 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+        
+        int a = 5;
+        while (a < 10){
+            System.out.println(a);
+            a++;
+        }
+        
+        int b = 8;
+        do {
+            System.out.println(b);
+            b++;
+        }while (b < 10);
+    }
+}
